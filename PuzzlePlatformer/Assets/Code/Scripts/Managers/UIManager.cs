b629d46bf167c0f87186a361818c1a2b005465a8
Turan <turan.dh@code.edu.az>
2023-01-14 06:47:04 -0800

    level 2 first lookdev

---
+++ PuzzlePlatformer/Assets/Code/Scripts/Managers/UIManager.cs
@@ -0,0 +1,14 @@
+using System;
+using DG.Tweening;
+
+namespace Code.Scripts.Managers
+{
+    public class UIManager : BaseManager<UIManager>
+    {
+        private void Start()
+        {
+            var seq = DOTween.Sequence();
+            seq.IsActive();
+        }
+    }
+}
